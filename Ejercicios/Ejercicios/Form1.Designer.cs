3acd5df6ee774a6dc60318c3a2462a1f69bb48ad
Arturo <Arturo_pdrzbn@hotmail.com>
2020-08-31 17:20:17 -0500

    ejercicio 1

---
+++ Ejercicios/Ejercicios/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Ejercicios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.HORAS = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblMontoNeto = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblMontoDescuento = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblMontoBruto = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.EMPLEADO = new System.Windows.Forms.Label();
+            this.txbEmpleado = new System.Windows.Forms.TextBox();
+            this.txbHoras = new System.Windows.Forms.TextBox();
+            this.txbCosto = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(225, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CONTROL DE PAGO DE EMPLEADOS";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // HORAS
+            // 
+            this.HORAS.AutoSize = true;
+            this.HORAS.Location = new System.Drawing.Point(66, 139);
+            this.HORAS.Name = "HORAS";
+            this.HORAS.Size = new System.Drawing.Size(45, 13);
+            this.HORAS.TabIndex = 1;
+            this.HORAS.Text = "HORAS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(324, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "COSTO POR HORA";
+            // 
+            // lblMontoNeto
+            // 
+            this.lblMontoNeto.AutoSize = true;
+            this.lblMontoNeto.Location = new System.Drawing.Point(362, 98);
+            this.lblMontoNeto.Name = "lblMontoNeto";
+            this.lblMontoNeto.Size = new System.Drawing.Size(35, 13);
+            this.lblMontoNeto.TabIndex = 3;
+            this.lblMontoNeto.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(362, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "MONTO NETO";
+            // 
+            // lblMontoDescuento
+            // 
+            this.lblMontoDescuento.AutoSize = true;
+            this.lblMontoDescuento.Location = new System.Drawing.Point(156, 98);
+            this.lblMontoDescuento.Name = "lblMontoDescuento";
+            this.lblMontoDescuento.Size = new System.Drawing.Size(35, 13);
+            this.lblMontoDescuento.TabIndex = 5;
+            this.lblMontoDescuento.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(156, 47);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(117, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "MONTO DESCUENTO";
+            // 
+            // lblMontoBruto
+            // 
+            this.lblMontoBruto.AutoSize = true;
+            this.lblMontoBruto.Location = new System.Drawing.Point(45, 98);
+            this.lblMontoBruto.Name = "lblMontoBruto";
+            this.lblMontoBruto.Size = new System.Drawing.Size(35, 13);
+            this.lblMontoBruto.TabIndex = 7;
+            this.lblMontoBruto.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(45, 47);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(88, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "MONTO BRUTO";
+            // 
+            // EMPLEADO
+            // 
+            this.EMPLEADO.AutoSize = true;
+            this.EMPLEADO.Location = new System.Drawing.Point(66, 108);
+            this.EMPLEADO.Name = "EMPLEADO";
+            this.EMPLEADO.Size = new System.Drawing.Size(66, 13);
+            this.EMPLEADO.TabIndex = 9;
+            this.EMPLEADO.Text = "EMPLEADO";
+            // 
+            // txbEmpleado
+            // 
+            this.txbEmpleado.Location = new System.Drawing.Point(149, 105);
+            this.txbEmpleado.Name = "txbEmpleado";
+            this.txbEmpleado.Size = new System.Drawing.Size(411, 20);
+            this.txbEmpleado.TabIndex = 10;
+            // 
+            // txbHoras
+            // 
+            this.txbHoras.Location = new System.Drawing.Point(149, 139);
+            this.txbHoras.Name = "txbHoras";
+            this.txbHoras.Size = new System.Drawing.Size(126, 20);
+            this.txbHoras.TabIndex = 11;
+            // 
+            // txbCosto
+            // 
+            this.txbCosto.Location = new System.Drawing.Point(434, 139);
+            this.txbCosto.Name = "txbCosto";
+            this.txbCosto.Size = new System.Drawing.Size(126, 20);
+            this.txbCosto.TabIndex = 12;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.lblMontoBruto);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.lblMontoDescuento);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.lblMontoNeto);
+            this.groupBox1.Location = new System.Drawing.Point(69, 227);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(491, 158);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "RESUMEN DE REPORTE";
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(69, 198);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(75, 23);
+            this.btnProcesar.TabIndex = 9;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(485, 198);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 14;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(69, 405);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 15;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 468);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txbCosto);
+            this.Controls.Add(this.txbHoras);
+            this.Controls.Add(this.txbEmpleado);
+            this.Controls.Add(this.EMPLEADO);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.HORAS);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label HORAS;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblMontoNeto;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblMontoDescuento;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblMontoBruto;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label EMPLEADO;
+        private System.Windows.Forms.TextBox txbEmpleado;
+        private System.Windows.Forms.TextBox txbHoras;
+        private System.Windows.Forms.TextBox txbCosto;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Ejercicios/Ejercicios/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            //Recuperando la informacion ingresada
+            string empleado = txbEmpleado.Text;
+            int horas = int.Parse(txbHoras.Text);
+            double costo = double.Parse(txbCosto.Text);
+
+            //Calculos
+            double sueldoBasico = horas * costo;
+            double bonificacion = 20.0 / 100 * sueldoBasico;
+            double sueldoBruto = sueldoBasico + bonificacion;
+            double descuento = sueldoBruto * 12.0 / 100;
+            double sueldoNeto = sueldoBruto - descuento;
+
+            // Imprimimos resultados 
+            lblMontoBruto.Text = sueldoBruto.ToString();
+            lblMontoDescuento.Text = descuento.ToString();
+            lblMontoNeto.Text = sueldoNeto.ToString();
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txbEmpleado.Clear();
+            txbHoras.Clear();
+            txbCosto.Clear();
+            lblMontoNeto.Text = "";
+            lblMontoBruto.Text = "";
+            lblMontoDescuento.Text = "";
+            txbEmpleado.Focus();
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("Esta seguro ?",
+                "Control de pago", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+            if(r == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
