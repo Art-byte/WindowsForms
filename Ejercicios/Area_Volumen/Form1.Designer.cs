baf6c0b7f00a35f6b94baa3cf217c1ff77090a73
Arturo <Arturo_pdrzbn@hotmail.com>
2020-08-31 19:41:14 -0500

    Area_volumen

---
+++ Ejercicios/Area_Volumen/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Area_Volumen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txbDiametro = new System.Windows.Forms.TextBox();
+            this.txbaAtura = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.txR = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(28, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(557, 102);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txbDiametro
+            // 
+            this.txbDiametro.Location = new System.Drawing.Point(163, 201);
+            this.txbDiametro.Name = "txbDiametro";
+            this.txbDiametro.Size = new System.Drawing.Size(118, 20);
+            this.txbDiametro.TabIndex = 1;
+            // 
+            // txbaAtura
+            // 
+            this.txbaAtura.Location = new System.Drawing.Point(28, 201);
+            this.txbaAtura.Name = "txbaAtura";
+            this.txbaAtura.Size = new System.Drawing.Size(118, 20);
+            this.txbaAtura.TabIndex = 2;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(163, 238);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(118, 23);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(343, 402);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(118, 23);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(467, 402);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(118, 23);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // txR
+            // 
+            this.txR.Location = new System.Drawing.Point(306, 201);
+            this.txR.Multiline = true;
+            this.txR.Name = "txR";
+            this.txR.Size = new System.Drawing.Size(279, 195);
+            this.txR.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "CONTROL DE AREA Y VOLUMEN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Altura";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(160, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Diametro";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txR);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txbaAtura);
+            this.Controls.Add(this.txbDiametro);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txbDiametro;
+        private System.Windows.Forms.TextBox txbaAtura;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.TextBox txR;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Ejercicios/Area_Volumen/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Area_Volumen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double altura = double.Parse(txbaAtura.Text);
+            double diametro = double.Parse(txbDiametro.Text);
+
+            double radio = diametro / 2;
+            double area = (2 * Math.PI * radio) * (radio + altura);
+            double volumen = Math.PI * Math.Pow(radio, 2) * altura;
+
+
+            //Mostrando datos
+            txR.Text = "** RESUMEN DE VOLUMEN Y AREAS **";
+            txR.Text += "\r\nValor altura: " + altura.ToString("0.00");
+            txR.Text += "\r\nValor diametro: " + diametro.ToString("0.00");
+            txR.Text += "\r\n---------------------------------------------";
+            txR.Text += "\r\nValor radio: " + radio.ToString("0.00");
+            txR.Text += "\r\nValor area: " + area.ToString("0.00");
+            txR.Text += "\r\nValor volumen: " + volumen.ToString("0.00");
+
+
+
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txbaAtura.Clear();
+            txbDiametro.Clear();
+            txR.Clear();
+            txbaAtura.Focus();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("Estas seguro de salir?", "Medidas", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information);
+
+            if(r == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
