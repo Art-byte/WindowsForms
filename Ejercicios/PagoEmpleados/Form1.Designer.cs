7ef10dbfe8517917dad7aee78a2c92f420e03d00
Arturo <Arturo_pdrzbn@hotmail.com>
2020-09-01 14:33:05 -0500

    Pagos

---
+++ Ejercicios/PagoEmpleados/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace PagoEmpleados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txbEmpleado = new System.Windows.Forms.TextBox();
+            this.lblSueldo = new System.Windows.Forms.Label();
+            this.cbCategoria = new System.Windows.Forms.ComboBox();
+            this.lvPagos = new System.Windows.Forms.ListView();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.Empleado = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Categoria = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Sueldo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Descuento = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Neto = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(282, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CONTROL DE PAGO DE EMPLEADOS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "FECHA DE REGISTRO";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(266, 77);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(35, 13);
+            this.lblFecha.TabIndex = 2;
+            this.lblFecha.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(97, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "NOMBRE DEL EMPLEADO";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(168, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "CATEGORIA";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(468, 155);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "SUELDO";
+            // 
+            // txbEmpleado
+            // 
+            this.txbEmpleado.Location = new System.Drawing.Point(269, 118);
+            this.txbEmpleado.Name = "txbEmpleado";
+            this.txbEmpleado.Size = new System.Drawing.Size(391, 20);
+            this.txbEmpleado.TabIndex = 7;
+            // 
+            // lblSueldo
+            // 
+            this.lblSueldo.AutoSize = true;
+            this.lblSueldo.Location = new System.Drawing.Point(553, 155);
+            this.lblSueldo.Name = "lblSueldo";
+            this.lblSueldo.Size = new System.Drawing.Size(34, 13);
+            this.lblSueldo.TabIndex = 8;
+            this.lblSueldo.Text = "$0.00";
+            // 
+            // cbCategoria
+            // 
+            this.cbCategoria.FormattingEnabled = true;
+            this.cbCategoria.Items.AddRange(new object[] {
+            "Jefe",
+            "Administrativo",
+            "Tecnico",
+            "Operario"});
+            this.cbCategoria.Location = new System.Drawing.Point(269, 152);
+            this.cbCategoria.Name = "cbCategoria";
+            this.cbCategoria.Size = new System.Drawing.Size(169, 21);
+            this.cbCategoria.TabIndex = 9;
+            this.cbCategoria.SelectedIndexChanged += new System.EventHandler(this.cbCategoria_SelectedIndexChanged);
+            // 
+            // lvPagos
+            // 
+            this.lvPagos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Empleado,
+            this.Categoria,
+            this.Sueldo,
+            this.Descuento,
+            this.Neto});
+            this.lvPagos.HideSelection = false;
+            this.lvPagos.Location = new System.Drawing.Point(100, 227);
+            this.lvPagos.Name = "lvPagos";
+            this.lvPagos.Size = new System.Drawing.Size(593, 165);
+            this.lvPagos.TabIndex = 10;
+            this.lvPagos.UseCompatibleStateImageBehavior = false;
+            this.lvPagos.View = System.Windows.Forms.View.Details;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(100, 198);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegistrar.TabIndex = 11;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(192, 198);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 12;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(100, 398);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 13;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Empleado
+            // 
+            this.Empleado.Text = "Empleado";
+            this.Empleado.Width = 114;
+            // 
+            // Categoria
+            // 
+            this.Categoria.Text = "Categoria";
+            this.Categoria.Width = 111;
+            // 
+            // Sueldo
+            // 
+            this.Sueldo.Text = "Sueldo";
+            this.Sueldo.Width = 103;
+            // 
+            // Descuento
+            // 
+            this.Descuento.Text = "Descuento";
+            this.Descuento.Width = 109;
+            // 
+            // Neto
+            // 
+            this.Neto.Text = "Neto";
+            this.Neto.Width = 123;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.lvPagos);
+            this.Controls.Add(this.cbCategoria);
+            this.Controls.Add(this.lblSueldo);
+            this.Controls.Add(this.txbEmpleado);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txbEmpleado;
+        private System.Windows.Forms.Label lblSueldo;
+        private System.Windows.Forms.ComboBox cbCategoria;
+        private System.Windows.Forms.ListView lvPagos;
+        private System.Windows.Forms.ColumnHeader Empleado;
+        private System.Windows.Forms.ColumnHeader Categoria;
+        private System.Windows.Forms.ColumnHeader Sueldo;
+        private System.Windows.Forms.ColumnHeader Descuento;
+        private System.Windows.Forms.ColumnHeader Neto;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Ejercicios/PagoEmpleados/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PagoEmpleados
+{
+    public partial class Form1 : Form
+    {
+        double sueldo = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblFecha.Text = DateTime.Today.Date.ToString("d");
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            string empleado = txbEmpleado.Text;
+            string categoria = cbCategoria.Text;
+
+            //Calculos
+            double descuento = 0;
+            if (sueldo > 2000) descuento = sueldo * (12.5 / 100);
+
+            double neto = sueldo - descuento;
+
+            //Imprimiendo resultados
+            ListViewItem fila = new ListViewItem(empleado);
+            fila.SubItems.Add(categoria);
+            fila.SubItems.Add(sueldo.ToString("C"));
+            fila.SubItems.Add(descuento.ToString("C"));
+            fila.SubItems.Add(neto.ToString("C"));
+            lvPagos.Items.Add(fila);
+
+            btnCancelar_Click(sender, e);
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("Esta seguro que desea salir?", "Control",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+            if(r == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            cbCategoria.Text = "(Seleccione)";
+            txbEmpleado.Clear();
+            txbEmpleado.Focus();
+        }
+
+        private void cbCategoria_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Capturamos valores de la categoria 
+            string categoria = cbCategoria.Text;
+
+            //Asignando de acuerdo a la categoria 
+            if (categoria == "Jefe") sueldo = 3500;
+            if (categoria == "Administrativo") sueldo = 2500;
+            if (categoria == "Tecnico") sueldo = 1700;
+            if (categoria == "Operario") sueldo = 1000;
+
+            lblSueldo.Text = sueldo.ToString("0.00");
+        }
+    }
+}
